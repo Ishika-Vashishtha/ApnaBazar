931d09dbb3f0b923ae36ab93b4a43a1b15800d15
Ishika-Vashishtha <146618041+Ishika-Vashishtha@users.noreply.github.com>
2024-01-18 10:42:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// var addtocart = document.getElementsByClassName("btn1")
+// console.log(addtocart)
+// for(var i=0 ; i < addtocart.length ; i++){
+//     var button = addtocart[i]
+//     button.addeventListener('click', function(event){
+//         var buttonClicked = event.target
+//         buttonClicked.parentElement.parentElement.parentElement.remove()
+//     })
+// }
+
+
+//  document.getElementsByClassName('hero-section1').addEventListener('click',hello());
+//  function hello(){
+//      window.open("https://flowbite.com/docs/components/alerts/");
+//     }
+//      ;
